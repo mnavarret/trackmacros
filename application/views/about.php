76d798112ff44d82d517879974d2a62f5a430b3b
mnavarret <matt.navarret@gmail.com>
2011-07-24 01:24:32 -0700

    First draft of about text.

--- application/views/about.php
+++ application/views/about.php
@@ -1,5 +1,15 @@
 	<section id="content" class="body">
-		<p>about</p>
+		<pre>
+	Trackmacros is your answer to the perfect diet! Through scientific research it has been found
+that body composition is a result of the individual's response to the ratio of macronutrient
+consumption.
+		
+	Register for free and we will give you the information on how to determine the macronutrient
+ratios for you to achieve your desired body composition and optium performance. No more fad
+diets, supplements that don't work, and frustation from not achieving your goals.</p>
+		
+	Sign up now and get the body you always wanted!
+		</pre>
 	</section><!-- /#content -->
 	
         <section id="bottom">
