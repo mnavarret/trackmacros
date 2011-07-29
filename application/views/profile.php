960911f9ab231b6c67d3cda4daeb3b3cdfb35615
mnavarret <matt.navarret@gmail.com>
2011-07-29 15:33:19 -0700

    Place holder for later.

---
+++ application/views/profile.php
@@ -0,0 +1,19 @@
+	<section id="content" class="body">
+		<p>profile coming soon.</p>
+	</section><!-- /#content -->
+	
+        <section id="bottom">
+	<section id="extras" class="body">
+		<div class="social">
+			<ul>
+				<li><a href="http://twitter.com/" rel="alternate"> </a></li>
+			</ul>
+		</div>
+      		
+		<div class="social">
+			<ul>
+				<li><a href="http://facebook.com/" rel="me"> </a></li>
+			</ul>
+		</div><!-- /.social -->
+	</section><!-- /#extras -->
+	</section>
