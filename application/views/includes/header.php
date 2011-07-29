0ea7d12cb6008f0c152f2a6a70521afec9dd8a1b
mnavarret <matt.navarret@gmail.com>
2011-07-29 15:36:16 -0700

    Formatting changes.

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -14,7 +14,22 @@
 		
 	<!--[if lt IE 7]>
 		<link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
-
+		
+<?php
+        $page = $this->uri->segment(2);
+        if( strcmp( $page, 'weight' ) == 0 )
+        {
+			  echo '<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>';
+			  echo '<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>';
+			  echo '<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>';
+			  
+			  echo '<script>';
+			  echo '$(document).ready(function() {';
+			  echo '	$("#datepicker").datepicker();';
+			  echo '});';
+			  echo '</script>';
+        }
+?>
 </head>
 
 <body>
