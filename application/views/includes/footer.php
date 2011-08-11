af892d5280af8416c634bddfd170bc7a0c28c791
mnavarret <matt.navarret@gmail.com>
2011-08-11 12:13:11 -0700

    Removed HTML5 style tags.

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,7 +1,5 @@
-	<footer id="contentinfo" class="body">
+	<div id="contentinfo" class="body">
 		<p>Website designed by <a href="http://matthew-navarret.com">Matthew Navarret</a>. Copyright 2011</p>
-	</footer>
-
-	<!-- /#contentinfo -->
+	</div><!-- /#contentinfo -->
 </body>
 </html>
